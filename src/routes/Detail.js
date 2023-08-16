174a30dc5102c5c0478bc87cafbe13c48b29adbb
89882 <pgh123166@naver.com>
2023-08-16 18:05:41 +0900

    test: async 제거

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -4,7 +4,7 @@ import { getUrlParam, navigate } from '../core/router';
 import { getMemberDetail } from '../store/memberStore';
 
 export default class Detail extends Component {
-  async render() {
+  render() {
     this.el.innerHTML = `
     <main class="detail">
     <div class='photo-detail skeleton'></div>
@@ -20,30 +20,30 @@ export default class Detail extends Component {
       </section>
       </main>`;
     this.el.prepend(new Header().el);
-    const member = await getMemberDetail(getUrlParam('id')); // 현재 url의 아이디를 가지고 member의 상세 데이터를 받아옴
-    this.el.innerHTML = `
-    <main class="detail">
-      <div class='photo-detail' style="background-image: url(${member.photoUrl})"></div>
-      <section class='information-container'>
-        <section class='information-title'>
-          INFORMATION
-          <button class='navigate-edit'>수정</button>
-        </section>
-        <p class='information'>
-          <span class='detail-category'>NAME</span>
-          <span class="detail-value">${member.name}</span>
-        </p>
-        <p class='information'>
-          <span class='detail-category'>EMAIL</span>
-          <span class="detail-value">${member.email}</span>
-        </p>
-      </section>
-    </main>
-      `;
-    this.el.prepend(new Header().el);
-    const navigateEditButton = this.el.querySelector('button');
-    navigateEditButton.addEventListener('click', () =>
-      navigate(`/edit?id=${member.id}`)
-    );
+    // const member =  getMemberDetail(getUrlParam('id')); // 현재 url의 아이디를 가지고 member의 상세 데이터를 받아옴
+    // this.el.innerHTML = `
+    // <main class="detail">
+    //   <div class='photo-detail' style="background-image: url(${member.photoUrl})"></div>
+    //   <section class='information-container'>
+    //     <section class='information-title'>
+    //       INFORMATION
+    //       <button class='navigate-edit'>수정</button>
+    //     </section>
+    //     <p class='information'>
+    //       <span class='detail-category'>NAME</span>
+    //       <span class="detail-value">${member.name}</span>
+    //     </p>
+    //     <p class='information'>
+    //       <span class='detail-category'>EMAIL</span>
+    //       <span class="detail-value">${member.email}</span>
+    //     </p>
+    //   </section>
+    // </main>
+    //   `;
+    // this.el.prepend(new Header().el);
+    // const navigateEditButton = this.el.querySelector('button');
+    // navigateEditButton.addEventListener('click', () =>
+    //   navigate(`/edit?id=${member.id}`)
+    // );
   }
 }
